4e31ab6ee46e683ad44be856de970e702823f287
Riccardo Drago <drago.riccardo@outlook.it>
2025-11-30 16:21:53 +0100

    Create response builder middleware

---
+++ backend/src/middleware/response-builder.ts
@@ -0,0 +1,40 @@
+import type { Request, Response, NextFunction } from "express";
+
+declare global {
+    namespace Express {
+        interface Response {
+            createResponse(
+                httpStatusCode: number,
+                message: string,
+                data?: unknown
+            ): Response;
+        }
+    }
+}
+
+export function createResponse<T>(
+    res: Response,
+    httpStatusCode: number,
+    message: string,
+    data?: T
+) {
+    return res.status(httpStatusCode).json({
+        success: httpStatusCode < 400,
+        message,
+        data,
+    });
+}
+
+const responseBuilder = (_: Request, res: Response, next: NextFunction) => {
+    res.createResponse = (
+        httpStatusCode: number,
+        message: string,
+        data?: unknown
+    ) => {
+        return createResponse(res, httpStatusCode, message, data);
+    };
+
+    next();
+};
+
+export default responseBuilder;
