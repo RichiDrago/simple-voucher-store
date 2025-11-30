ee2b0f2357ad56a25899c1f287ba7a4c5fe863e6
Riccardo Drago <drago.riccardo@outlook.it>
2025-11-30 18:22:18 +0100

    Uodate role model

--- backend/src/model/Role.model.ts
+++ backend/src/model/Role.model.ts
@@ -1,18 +1,41 @@
-import { DataTypes } from "sequelize";
+import { DataTypes, Model } from "sequelize";
 
 import db from "../config/db.js";
 
-const Role = db.define("Role", {
-    name: {
-        type: DataTypes.STRING,
-        primaryKey: true,
-        allowNull: false,
+export interface RoleAttributes {
+    name: string;
+    privilege: number;
+}
+
+export interface RoleCreationAttributes extends RoleAttributes {}
+
+export class Role
+    extends Model<RoleAttributes, RoleAttributes>
+    implements RoleAttributes
+{
+    declare name: string;
+    declare privilege: number;
+    declare createdAt: string;
+    declare updatedAt: string;
+}
+
+Role.init(
+    {
+        name: {
+            type: DataTypes.STRING,
+            primaryKey: true,
+            allowNull: false,
+        },
+        privilege: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            defaultValue: 1,
+        },
     },
-    privilege: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        defaultValue: 1,
-    },
-});
+    {
+        sequelize: db,
+        tableName: "Role",
+    }
+);
 
 export default Role;
